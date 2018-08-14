fcf14059cb402fba89135e318ee32ffa1c057717
marcelaschacon <marcel.alves.lem@hotmail.com>
2018-08-14 18:02:09 -0300

    Atualizacao aula

--- src/main/java/br/com/db1/App.java
+++ src/main/java/br/com/db1/App.java
@@ -1,13 +1,68 @@
 package br.com.db1;
 
+import java.math.BigDecimal;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
+public class App {
+	public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        int zero = 0;
+        Integer zeroObjeto = 0;
+        zeroObjeto.doubleValue();
+        
+        char vogal = 'a';
+        String vogalString = "a";
+        vogalString.trim();
+        
+        double umPontoDois = 1.2;
+        Double valor = 1.2;
+        Double valor2 = 2.0;
+        Float valor3 = 3f;
+        
+        BigDecimal dinheiro = BigDecimal.ZERO; 
+        
+        for (Integer x = 0; x < 100; x++){         }
+        
+        Integer x = 0;
+        while (x<100){
+        	
+        	x++; }
+        
+        do{
+        	while(x<100);
+        	
+        x % 2 == 0: "eh par" ? "eh impar";
+        
+        switch (x){
+        case 0;
+        	System.out.println("zero");
+        	break;
+        }
+        
+        if(){
+        else if(){
+        	
+        else{   
+        	
+       
+        int[] matriz = {1,2,3,4};
+        int matriz22 = 1;
+        
+        Integer[] matrizInteger = {1,2,3,4};
+        
+        List<Integer> valores = new ArrayList<Integer>();
+        List<Integer> valoresLinkado = new LinkedList<Integer>();
+        Set<Integer> valoresNaoRepetidos = new HashSet<Integer>();
+        List<String> nomes;
+        List<Dinheiro> meuSalario;
+        
+        		
     }
+        	
 }
+}}}}
---
+++ src/main/java/br/com/db1/type/EstadosDoSulDoBrasil.java
@@ -0,0 +1,7 @@
+package br.com.db1.type;
+
+public enum EstadosDoSulDoBrasil {
+	
+	PARANA, SANTA_CATARINA, RIO_GRANDE_DO_SUL;
+
+}

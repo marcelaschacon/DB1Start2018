0e92d583a72b81f121daff9c759518577e6bd551
marcelaschacon <marcel.alves.lem@hotmail.com>
2018-08-22 17:32:53 -0300

    Aula10 fim

---
+++ src/main/java/br/com/db1/orientacaoaobjetos/PessoaFisica.java
@@ -0,0 +1,22 @@
+package br.com.db1.orientacaoaobjetos;
+
+public class PessoaFisica extends Pessoa {
+	private String cpf;
+	
+	public PessoaFisica() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	public Boolean validadorCpf(){
+		return true;	
+	}
+
+}
---
+++ src/main/java/br/com/db1/orientacaoaobjetos/PessoaJuridica.java
@@ -0,0 +1,22 @@
+package br.com.db1.orientacaoaobjetos;
+
+public class PessoaJuridica extends Pessoa {
+	private String cnpj;
+	
+	public PessoaJuridica() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	
+	public Boolean validadorCnpj(){
+		return true;	
+	}
+
+}
